26467787a8d43b3254f4f40724aa9cdc8e9b64a1
zheng861014 <zheng861014@gmail.com>
2013-07-08 11:15:05 +0800

    调整代码格式

--- src/main/java/org/n3r/jedis/RunDelayQ.java
+++ src/main/java/org/n3r/jedis/RunDelayQ.java
@@ -6,24 +6,22 @@ import java.util.TimerTask;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+public class RunDelayQ extends TimerTask {
 
-public class RunDelayQ extends TimerTask{
-	
-	
 	public void run() {
 		Logger logger = LoggerFactory.getLogger(RunDelayQ.class);
-         EcRocJedis jedis = new EcRocJedis("132.35.81.197",6379);
-    	  jedis.setDelay("delayqueue", "remark", 30);
-    	  logger.info("setDelayQ operate");
-    	  Set<String> response = jedis.getDelay("delayqueue");
-    	  logger.info(response.toString());
-    	  try {
+		EcRocJedis jedis = new EcRocJedis("132.35.81.197", 6379);
+		jedis.setDelay("delayqueue", "remark", 30);
+		logger.info("setDelayQ operate");
+		Set<String> response = jedis.getDelay("delayqueue");
+		logger.info(response.toString());
+		try {
 			Thread.sleep(30000);
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
-    	  response = jedis.getDelay("delayqueue");
-    	  logger.info(response.toString());
-      
+		response = jedis.getDelay("delayqueue");
+		logger.info(response.toString());
+
 	}
 }
